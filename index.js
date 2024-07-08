db0ace4b591b92d50e07c839fb2e8afc4a81082b
Alex M. - Clockwork <alex@clockwork.gr>
2024-07-08 15:45:47 +0300

    wip workflows

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const submissionDate = context.event.issue.created_at;
 
 const template_fields = context.event.issue.body.split("### ").slice(1);
 const addChainPR = (fields) => {
-    const chainId = fields[1].split("\n\n")[1];
-    const note = fields[2].split("\n\n").slice(1,-1);
-    const threatLevel=fields[3].split("\n\n")[1][0];
+    const chainId = fields[0].split("\n\n")[1];
+    const note = fields[1].split("\n\n").slice(1,-1);
+    const threatLevel=fields[2].split("\n\n")[1][0];
     console.log( {chainId,note,threatLevel});
 }
 if (context.event.issue.title.startsWith("[Chain-add]: ")) {
