f9973523d5c650d44a2bc7ab7035756efb4000e6
Alex M. - Clockwork <alex@clockwork.gr>
2024-07-08 19:21:07 +0300

    wip: workflows

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 const context = JSON.parse(process.env.GITHUB_CONTEXT);
 const submitter = context.actor;
 const submissionDate = context.event.issue.created_at;
@@ -5,8 +7,29 @@ const submissionDate = context.event.issue.created_at;
 const template_fields = context.event.issue.body.split("### ").slice(1);
 const addChainPR = (fields) => {
     const chainId = fields[0].split("\n\n")[1];
-    const note = fields[1].split("\n\n").slice(1,-1);
+    const note = fields[1].split("\n\n").slice(1,-1).join("\r\n");
     const threatLevel=fields[2].split("\n\n")[1][0];
+    const blacklist = JSON.parse(fs.readFileSync('chain-blacklist.json', 'utf8'));
+    const exists = blacklist.find(x => x.chainId ==chainId);
+    if (exists>=0) {
+        blacklist[exists].userNotes.push({
+            userName: submitter,
+            userNote: note,
+            noteDate: submissionDate
+        })
+    }else{
+        blacklist.push({
+            chainId,
+            submissionDate,
+            threatLevel,
+            userNotes: [{
+                userName: submitter,
+                userNote: note,
+                noteDate: submissionDate
+            }]
+        })
+    }
+    fs.writeFileSync("chain-blacklist.json", JSON.stringify(blacklist));
     console.log( {chainId,note,threatLevel});
 }
 if (context.event.issue.title.startsWith("[Chain-add]: ")) {
