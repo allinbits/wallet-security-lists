5b4de0088acf902eeeef21dcf9772c59d2e28376
Alex M. - Clockwork <alex@clockwork.gr>
2024-07-08 15:43:08 +0300

    wip: workflows

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ const context = JSON.parse(process.env.GITHUB_CONTEXT);
 const submitter = context.actor;
 const submissionDate = context.event.issue.created_at;
 
-const template_fields = context.event.issue.body.split("### ");
-console.log(template_fields);
+const template_fields = context.event.issue.body.split("### ").slice(1);
+if (context.event.issue.title.startsWith("[Chain-add]: ")) {
+    addChainPR(template_fields);
+}
+if (context.event.issue.title.startsWith("[Site-add]: ")) {
+    addSitePR(template_fields);
+}
+if (context.event.issue.title.startsWith("[Asset-add]: ")) {
+    addAssetPR(template_fields);
+}
+const addChainPR = (fields) => {
+    const chainId = fields[1].split("\n\n")[1];
+    const note = fields[2].split("\n\n").slice(1,-1);
+    const threatLevel=fields[3].split("\n\n")[1][0];
+    console.log( {chainId,note,threatLevel});
+}
